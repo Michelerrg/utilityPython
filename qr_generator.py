bfb2467f685eff28afdefdbcd3a9975fa4ef933e
Michele <98821855+Michelerrg@users.noreply.github.com>
2024-07-22 15:36:10 +0200

    aggiunta while

--- qr_generator.py
+++ qr_generator.py
@@ -5,13 +5,20 @@ import qrcode
 
 def generate_qr(web_site, file_name):
     try:
-        img = qrcode.make(web_site)
+        img = qrcode.make(
+            web_site,
+            box_size=20,
+            )
         img.save(file_name)
         print(f'QR salvato correttamente come {file_name}')
     except Exception as ex:
         print(f'ERROR: {ex}')
 
-input_web = input('Scrivi qui il sito web: ')
-nome_qr = input('Scrivi il nome del file: ') + '.png'
+while True:
+    go_on = input('Scrivi "exit" per uscire o premi invio: ')
+    if go_on == "exit":
+        break
+    input_web = input('Scrivi qui il sito web: ')
+    nome_qr = input('Scrivi il nome del file: ') + '.png'
 
-generate_qr(input_web, nome_qr)
+    generate_qr(input_web, nome_qr)
